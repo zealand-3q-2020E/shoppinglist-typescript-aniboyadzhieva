09c53ecafba824bc390588d87a970bc21b8b99f8
aniboyadzhieva <anix0072@edu.easj.dk>
2020-09-22 15:23:48 +0200

    Task 1

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,20 @@
 //Write your Typescript code here
+
+//task 1 
+let myElement:HTMLLIElement = <HTMLLIElement> document.getElementById("0");
+console.log(myElement);
+
+
+let myList:HTMLCollectionOf<HTMLLIElement> =
+    <HTMLCollectionOf<HTMLLIElement>> document.getElementsByClassName("unhealthy");
+    console.log(myList);
+
+//var myList = document.getElementsByClassName("healthy");
+//console.log(myList);
+
+let myElements2:HTMLCollectionOf<HTMLLIElement> =
+    <HTMLCollectionOf<HTMLLIElement>> document.getElementsByTagName("li");
+    console.log(myElements2);
+
+
+
